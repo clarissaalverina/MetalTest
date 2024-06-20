cf07464b9ed147c4b01eed88f84a43592190a998
Clarissa Alverina <clarissaalverina97@gmail.com>
2024-06-20 20:30:07 +0700

    Initial Commit

---
+++ MetalTest/MetalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTestApp.swift
+//  MetalTest
+//
+//  Created by Clarissa Alverina on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
